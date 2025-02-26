a355330da6fdca3632d269972e849bf4e83f5158
Dcatna <ddcatena@gmail.com>
2025-02-25 23:41:44 -0500

    .

--- TabTransferWeb/src/main.tsx
+++ TabTransferWeb/src/main.tsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
       { path: "/home", element:<ProtectedRoute><Home /></ProtectedRoute>  },
       { path: "/group/:id", element: <ProtectedRoute><Group /></ProtectedRoute> },
       { path: "/forgotpassword", element: <ForgotPassword /> },
-      { path: "/resetpassword:token", element: <ChangePasswordPage /> },
+      { path: "/resetpassword/:token", element: <ChangePasswordPage /> },
 
     ]
   }
