567394b2e6e81147527f201b5c23f2687914f0a6
ddcat <ddcatena@gmail.com>
2025-04-21 18:46:18 -0400

    .

--- TabTransferWeb/src/components/Group.tsx
+++ TabTransferWeb/src/components/Group.tsx
@@ -214,7 +214,7 @@ const AddTabDialog = ({ group_name, refreshList }: AddTabDialogProp) => {
               id="Url"
               value={url}
               onChange={(e) => setUrl(e.target.value)}
-              className="mt-1 block w-full rounded-md shadow-sm"
+              className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
               placeholder="Enter the url for the tab"
               required
             />
@@ -230,7 +230,7 @@ const AddTabDialog = ({ group_name, refreshList }: AddTabDialogProp) => {
               id="title"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
-              className="mt-1 block w-full rounded-md shadow-sm"
+              className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
               placeholder="Enter a title for the tab"
             />
           </div>
